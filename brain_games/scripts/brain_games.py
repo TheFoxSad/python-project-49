69992cfb1a802b2deccd665385be984a588b241e
Zakir <yes.its.me.zak@gmail.com>
2023-02-26 12:38:20 +0300

    updated brain_games/scripts/brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+from brain_games import cli
+
 
 def greeting():
-    print(f"Welcome to the Brain Games!")
+    print(f'Welcome to the Brain Games!')
 
 
 def main():
     greeting()
-
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
+
