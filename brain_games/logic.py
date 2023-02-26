8cc9b5e1170cca754e5d26a7e1323f4c9a2f11d7
Zakir <yes.its.me.zak@gmail.com>
2023-02-26 17:11:10 +0300

    fixed brain_games/logic.py

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -1,26 +1,22 @@
 import prompt
-import random
 
-def even_parity():
+
+def start_game(game):
     print(f'Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
-    print(f'Answer "yes" if the number is even, otherwise answer "no".')
-    count_question = 0
-    while  count_question < 3:
-        question_num = random.randint(1, 100)
-        if question_num % 2 == 0:
-            check_even = 'yes'
-        elif question_num %2 != 0:
-            check_even = 'no'
-        print(f'Question: {question_num}')
+    game.rules()
+    current_round = 0
+    while  current_round < game.rounds:
+        question, right_answer = game.random_data()
+        print(f'Question: {question}')
         answer = input('Your answer: ')
-        if check_even == answer:
-            print(f'Correct')
-            count_question += 1
-        elif check_even != answer:
-            print(f""""{answer}" is wrong answer ;(. Correct answer was "{check_even}".
+        if right_answer == answer:
+            print(f'Correct!')
+            current_round += 1
+        elif right_answer != answer:
+            print(f""""{answer}" is wrong answer ;(. Correct answer was "{right_answer}".
 Let's try again, {name}!""")
             break
-        if count_question == 3:
+        if current_round == game.rounds:
             print(f'Congratuletion, {name}!')
