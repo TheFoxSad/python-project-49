f20cf1c67e8323deeb94df534e2ee3a5d58cbecd
Zakir <yes.its.me.zak@gmail.com>
2023-03-01 19:44:14 +0300

    added brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games import logic
+from brain_games.games import progression
+
+
+def main():
+    logic.start_game(progression)
+
+
+if __name__ == '__main__':
+    main()
