3da718e22fbc2705fbda65f0ba17202e5c6edf60
Zakir <yes.its.me.zak@gmail.com>
2023-03-02 21:12:27 +0300

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -10,9 +10,11 @@ def random_data():
     question = random.randint(1, 100)
     if question < 2:
         right_answer = "no"
+        return question, right_answer
     for i in range(2, int(math.sqrt(question) + 1)):
         if question % i == 0:
             right_answer = "no"
-        else:
-            right_answer = "yes"
-    return question, right_answer
+            return question, right_answer
+    else:
+        right_answer = "yes"
+        return question, right_answer
