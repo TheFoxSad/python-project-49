425893a385eefb7e84ae910e0f2a56c04c17c263
Zakir <yes.its.me.zak@gmail.com>
2023-03-03 21:12:10 +0300

    fixed file even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,15 +1,16 @@
 import random
 
-rounds = 3
+ROUNDS = 3
+
 
 def rules():
-    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
 
 def random_data():
     question = random.randint(0, 100)
     if question % 2 == 0:
         right_answer = 'yes'
-    elif question %2 != 0:
+    elif question % 2 != 0:
         right_answer = 'no'
     return question, right_answer
-
