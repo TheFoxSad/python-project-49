0f5f41e5e462d893f0b6eb2292936ebf9fd5c47c
Zakir <yes.its.me.zak@gmail.com>
2023-03-06 18:57:14 +0300

    fixed

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-import sys
 from brain_games import logic
-from brain_games.games import calc, even, prime, gcd, progression
+from brain_games.games import even
 
-
-my_dict = {"brain-calc"        : calc,
-           "brain-prime"       : prime,
-           "brain-gcd"         : gcd,
-           "brain-progression" : progression,
-           "brain-even"        : even}
-
+value = even
 
 def main():
-    value = my_dict.get(sys.argv[0])
     logic.start_game(value)
+
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from brain_games.games.games import main
+from brain_games.games import prime
+
 
 if __name__ == '__main__':
     main()
