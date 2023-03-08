ffaa008c52f792910d84dbababa4fe7eac5a3294
Zakir <yes.its.me.zak@gmail.com>
2023-03-08 22:50:55 +0300

    fixed

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,10 +6,9 @@ RULES = 'What is the result of the expression?'
 
 def get_question_and_right_answer():
     operators = {
-	"+" : operator.add,
-	"-" : operator.sub,
-	"*" : operator.mul
-}
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul}
     op_name = random.choice(["+", "-", "*"])
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
