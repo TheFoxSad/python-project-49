0e45f560b8298c0a18c28a00b589973cec78c6d3
Zakir <yes.its.me.zak@gmail.com>
2023-03-08 21:02:59 +0300

    updated calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,17 +1,19 @@
 import random
+import operator
 
 RULES = 'What is the result of the expression?'
 
 
 def random_data():
-    operator = random.choice(["+", "-", "*"])
-    first_num = random.randint(1, 10)
-    second_num = random.randint(1, 10)
-    question = f"{first_num} {operator} {second_num}"
-    if operator == "+":
-        right_answer = first_num + second_num
-    elif operator == "-":
-        right_answer = first_num - second_num
-    elif operator == "*":
-        right_answer = first_num * second_num
+    operators = {
+	"+" : operator.add,
+	"-" : operator.sub,
+	"*" : operator.mul
+}
+    op_name = random.choice(["+", "-", "*"])
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+    question = f"{num1} {op_name} {num2}"
+    if op_name in operators:
+        right_answer = operators[op_name](num1, num2)
     return question, str(right_answer)
