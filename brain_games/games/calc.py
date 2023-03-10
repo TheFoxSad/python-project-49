b9e60bd724053b9bbcdd05fcc50caecb92f76b33
Zakir <yes.its.me.zak@gmail.com>
2023-03-10 19:49:11 +0300

    fixed

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -13,6 +13,5 @@ def get_question_and_right_answer():
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
     question = f"{num1} {op_name} {num2}"
-    if op_name in operators:
-        right_answer = operators[op_name](num1, num2)
+    right_answer = operators[op_name](num1, num2)
     return question, str(right_answer)
